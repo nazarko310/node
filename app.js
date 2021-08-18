8f3142e9f3986cded8a42ef0b3093a9e6fdd50aa
nazarko310 <nazar2000pok@gmail.com>
2021-08-18 14:43:42 +0300

    lessons1_practice

--- app.js
+++ app.js
@@ -1,90 +1,67 @@
-//перемістити всі файли з вкладених папок в іншу папку на одному рівні
+// - у вас є масив юзрів (до 10), з такими полями наприклад
+// , вам потрібно написати метод який створює файлики - де назва файлику - це імя вашого юзера (наприклад - Olya.txt),
+// вміст це сам ваш юзер - { name: 'olya', gender: 'female', age: 20 }
+// перед тим створити 4 папки - наприклад - manOlder20, manYounger20, womanOlder20, womanYounger20
+// і розподілити ваших юзерів саме по відповідних папках
 
 const fs = require('fs');
 const path = require('path');
 
-const dirPathDir = path.join(__dirname, 'main', 'dir1', 'dir1_1');
-const dirPathDir2 = path.join(__dirname, 'main', 'dir2', 'dir2_2')
-const mainDir = path.join(__dirname, 'main');
 
-fs.mkdir(dirPathDir, {recursive: true}, errWithCreateDir => {
-    console.log(errWithCreateDir);
+const dirPath = path.join(__dirname, 'main')
+const dirManOlder20 = path.join(dirPath, 'manOlder20');
+const dirManYounger20 = path.join(dirPath, 'manYounger20');
+const dirWomanOlder20 = path.join(dirPath, 'womanOlder20');
+const DirWomanYounger20 = path.join(dirPath, 'womanYounger20');
+
+
+fs.mkdir(dirManOlder20, {recursive: true}, err => {
+    console.log(err);
 })
-fs.mkdir(dirPathDir2, {recursive: true}, errWithCreateDir => {
-    console.log(errWithCreateDir);
+fs.mkdir(dirManYounger20, {recursive: true}, err => {
+    console.log(err);
+})
+fs.mkdir(dirWomanOlder20, {recursive: true}, err => {
+    console.log(err);
+})
+fs.mkdir(DirWomanYounger20, {recursive: true}, err => {
+    console.log(err);
 })
 
-fs.readdir(mainDir, (errWithReadMainDir, filesInMainDir) => {
-    if (errWithReadMainDir) {
-        console.log(errWithReadMainDir);
-        return;
-    }
-    filesInMainDir.forEach(fileInMainDir => {
-        const fileToReadInMainDir = path.join(mainDir, fileInMainDir)
-        fs.stat(fileToReadInMainDir, (errStatMain, statsMain) => {
-            if (errStatMain) {
-                console.log(errStatMain);
-                return;
-            }
-            if (statsMain.isFile()) {
-                fs.rename(path.join(mainDir, fileInMainDir), path.join(mainDir, fileInMainDir), errWithRenameMain => {
-                    if (errWithRenameMain) {
-                        console.log(errWithRenameMain);
-                        return;
-                    }
-                    console.log('Good')
-                })
-            } else {
-                fs.readdir(path.join(mainDir, fileInMainDir), (errorWithReadInnerDir, filesInInnerDir) => {
-                    if (errorWithReadInnerDir) {
-                        console.log(errorWithReadInnerDir);
-                        return;
-                    }
-                    filesInInnerDir.forEach(fileInInnerDir => {
-                        const fileToReadInnerDir = path.join(mainDir, fileInMainDir, fileInInnerDir)
-                        fs.stat(fileToReadInnerDir, (errWithStatInInnerDir, statsInnerDir) => {
-                            if (errWithStatInInnerDir) {
-                                console.log(errWithStatInInnerDir);
-                                return;
-                            }
-                            if (statsInnerDir.isFile()) {
-                                fs.rename(fileToReadInnerDir, path.join(mainDir, fileInInnerDir), errAfterRenameInnerDir => {
-                                    if (errAfterRenameInnerDir) {
-                                        console.log(errAfterRenameInnerDir);
-                                        return;
-                                    }
-                                    console.log('good')
-                                })
-                            } else {
-                                fs.readdir(fileToReadInnerDir, (errInNextDir, filesInNextDir) => {
-                                    if (errInNextDir) {
-                                        console.log(errInNextDir);
-                                        return;
-                                    }
-                                    filesInNextDir.forEach(fileInNextDir => {
-                                        const filePathToNextDir = path.join(fileToReadInnerDir, fileInNextDir);
-                                        fs.stat(filePathToNextDir, (errWithStatInNextDir, statsInNextDir) => {
-                                            if (errWithStatInNextDir) {
-                                                console.log(errWithStatInNextDir);
-                                                return;
-                                            }
-                                            if (statsInNextDir.isFile()) {
-                                                fs.rename(filePathToNextDir, path.join(mainDir, fileInNextDir), errWithRenameFileInNextDir => {
-                                                    if (errWithRenameFileInNextDir) {
-                                                        console.log(errWithRenameFileInNextDir);
-                                                        return;
-                                                    }
-                                                    console.log('good')
-                                                })
-                                            }
-                                        })
-                                    })
-                                })
-                            }
-                        })
-                    })
-                })
-            }
+fs.mkdir(dirPath, {recursive: true}, err => {
+    console.log(err);
+})
+
+const users = [
+    {name: "Olya", gender: "female", age: 21},
+    {name: "Valya", gender: "female", age: 22},
+    {name: "Natasha", gender: "female", age: 23},
+    {name: "Lena", gender: "female", age: 14},
+    {name: "Ulya", gender: "female", age: 15},
+    {name: "Vasya", gender: "male", age: 21},
+    {name: "Petya", gender: "male", age: 22},
+    {name: "Sasha", gender: "male", age: 23},
+    {name: "Seryu", gender: "male", age: 14},
+    {name: "Vitalik", gender: "male", age: 15}
+]
+
+
+for (let i = 0; i < users.length; i++) {
+    if (users[i].age >= 20 && users[i].gender === 'male') {
+        fs.appendFile(path.join(dirManOlder20, `${users[i].name}.txt`), JSON.stringify(users[i]), err => {
+            console.log(err);
         })
-    })
-})
+    } else if (users[i].age <= 20 && users[i].gender === 'male') {
+        fs.appendFile(path.join(dirManYounger20, `${users[i].name}.txt`), JSON.stringify(users[i]), err => {
+            console.log(err);
+        })
+    } else if (users[i].age <= 20 && users[i].gender === 'female') {
+        fs.appendFile(path.join(DirWomanYounger20, `${users[i].name}.txt`), JSON.stringify(users[i]), err => {
+            console.log(err);
+        })
+    } else if (users[i].age >= 20 && users[i].gender === 'female') {
+        fs.appendFile(path.join(dirWomanOlder20, `${users[i].name}.txt`), JSON.stringify(users[i]), err => {
+            console.log(err);
+        })
+    }
+}
