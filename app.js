58af0ba84ca355472093d961f29ba656d5f85b34
nazarko310 <nazar2000pok@gmail.com>
2021-08-18 11:39:59 +0300

    lessons1

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const path = require('path');
 const folderPath18 = path.join(__dirname, 'lessons', '18_00');
 const folderPath20 = path.join(__dirname, "lessons", '20_00')
 const filePathOksana = path.join(folderPath18, 'oksana.txt')
-const filePathAndriy = path.join(folderPath18, 'andriy.js')
-const filePathDima = path.join(folderPath20, 'dima.js')
-const filePathMasha = path.join(folderPath20, 'masha.js')
+const filePathAndriy = path.join(folderPath18, 'igor.txt')
+const filePathIgor = path.join(folderPath20, 'igor.txt')
+const filePathMasha = path.join(folderPath20, 'masha.txt')
 
 
 fs.mkdir(folderPath18, {recursive: true}, err => {
@@ -16,48 +16,86 @@ fs.mkdir(folderPath20, {recursive: true}, err => {
     console.log(err);
 })
 
-// const user_Oksana = {
-//     name: "Oksana",
-//     gender: "female"
-// }
-// const user_Andriy = {
-//     name: "Andriy",
-//     gender: "male"
-// }
-//
-// const user_Dima = {
-//     name: "Dima",
-//     gender: "male"
-// }
-//
-// const user_Masha = {
-//     name: "Masha",
-//     gender: "female"
-// }
-//
-// fs.appendFile(filePathOksana, `const user_Oksana = {name: "Oksana", gender: "female"}`, err => {
-//     console.log(err);
-// })
-// fs.appendFile(filePathAndriy, `${(JSON.stringify(user_Andriy))}`, err => {
-//     console.log(err);
-// })
-// fs.appendFile(filePathDima, `${(JSON.stringify(user_Dima))}`, err => {
-//     console.log(err)
-// })
-// fs.appendFile(filePathMasha, `${(JSON.stringify(user_Masha))}`, err => {
-//     console.log(err)
-// })
+const user_Oksana = {
+    name: "Oksana",
+    gender: "female"
+}
+const user_Andriy = {
+    name: "Andriy",
+    gender: "male"
+}
 
-fs.readFile(filePathOksana, (err, data) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    const dataFromUser = data.toString();
-    if(dataFromUser.includes('female')){
-        console.log('good')
-    }
+const user_Igor = {
+    name: "Igor",
+    gender: "male"
+}
+
+const user_Masha = {
+    name: "Masha",
+    gender: "female"
+}
+
+fs.appendFile(filePathOksana, `${(JSON.stringify(user_Oksana))}`, err => {
+    console.log(err);
+})
+fs.appendFile(filePathAndriy, `${(JSON.stringify(user_Andriy))}`, err => {
+    console.log(err);
+})
+fs.appendFile(filePathIgor, `${(JSON.stringify(user_Igor))}`, err => {
+    console.log(err)
+})
+fs.appendFile(filePathMasha, `${(JSON.stringify(user_Masha))}`, err => {
+    console.log(err)
 })
 
+fs.readdir(folderPath18, (errWithReadDir, files) => {
+    if (errWithReadDir) {
+        console.log(errWithReadDir);
+        return;
+    }
+    files.forEach(file => {
+        fs.readFile(path.join(folderPath18, file), (errWithReadFile, data) => {
+            if (errWithReadFile) {
+                console.log(errWithReadFile);
+                return;
+            }
+            const dataFromFile = data.toString()
+            if (!(dataFromFile.match('female'))) {
+                fs.rename(path.join(folderPath18, file), path.join(folderPath20, file), errWithRename => {
+                    if (errWithRename) {
+                        console.log(errWithRename);
+                        return;
+                    }
+                    console.log('Complete')
+                })
+            }
+        })
+    })
+})
+
+fs.readdir(folderPath20, (errorWithReadDir, files) => {
+    if(errorWithReadDir){
+        console.log(errorWithReadDir);
+        return;
+    }
+    files.forEach(file=>{
+        fs.readFile(path.join(folderPath20,file), (errorWithRead, data) => {
+            if(errorWithRead){
+                console.log(errorWithRead);
+                return;
+            }
+            const dataFromFile = data.toString();
+            if(dataFromFile.match('female')){
+                fs.rename(path.join(folderPath20, file), path.join(folderPath18, file), errorWithRename =>{
+                    if (errorWithRename){
+                        console.log(errorWithRename);
+                        return;
+                    }
+                    console.log('Complete')
+                } )
+            }
+        })
+    })
+})
 
 
